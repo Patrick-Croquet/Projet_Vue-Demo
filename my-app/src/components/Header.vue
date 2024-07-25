d3e3015d11a6a47e4c154b6881075ff242cca760
Patrick-Croquet <58164643+Patrick-Croquet@users.noreply.github.com>
2024-07-25 09:43:21 +0200

    Initial Commit

--- my-app/src/components/Header.vue
+++ my-app/src/components/Header.vue
@@ -14,7 +14,7 @@
     </div>
 
     <div class="container mx-auto py-12">
-        <h1 class="text-3xl font-bold text-gray-800">Mon application Vue.js avec Tailwind</h1>
+        <h1 class="text-3xl font-bold text-gray-800">Mon application Vue3.js avec Tailwind</h1>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Bouton Tailwind</button>
     </div>
 </template>
